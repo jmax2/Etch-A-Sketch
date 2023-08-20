ac1eed9a7b8198fc42ff6db2fbbe3a6f04c685a9
jmax2 <142146891+jmax2@users.noreply.github.com>
2023-08-20 09:59:55 +0200

    end

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
 const container = document.querySelector('.container');
+let squaresPerSide = 16; // Default value
 
-// Create 16x16 grid of divs
-for (let i = 0; i < 16 * 16; i++) {
-    const div = document.createElement('div');
-    div.classList.add('grid-square');
-    container.appendChild(div);
+// Create initial grid
+createGrid(squaresPerSide);
+
+// Function to create grid
+function createGrid(squaresPerSide) {
+    container.style.setProperty('--squares-per-side', squaresPerSide);
+    container.innerHTML = ''; // Clear existing grid
+
+    // Generate new grid of divs
+    for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+        const div = document.createElement('div');
+        div.classList.add('grid-square');
+        container.appendChild(div);
+
+        // Add event listener for hover effect
+        div.addEventListener('mouseover', function () {
+            div.style.backgroundColor = getRandomColor();
+        });
+    }
+}
+
+// Function to generate random color
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Function to prompt user for number of squares per side
+function generateGrid() {
+    const userInput = prompt('Enter the number of squares per side (maximum: 100):');
+    const newSquaresPerSide = parseInt(userInput);
+
+    if (newSquaresPerSide && newSquaresPerSide > 0 && newSquaresPerSide <= 100) {
+        squaresPerSide = newSquaresPerSide;
+        createGrid(squaresPerSide);
+    } else {
+        alert('Invalid input! Please enter a number between 1 and 100.');
+    }
 }
